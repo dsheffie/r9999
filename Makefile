e46467de01d8f210b7ec435cf512d57e1868c9e9
David Sheffield <sheffield.david@gmail.com>
2026-04-25 07:39:36 -0700

    script convert to verilog

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ OBJ = top.o verilated.o verilated_vcd_c.o loadelf.o interpret.o disassemble.o he
 SV_SRC = core_l1d_l1i.sv core.sv exec.sv decode_mips32.sv shiftregbit.sv shift_right.sv mul.sv find_first_set.sv divider.sv l1d.sv l1i.sv machine.vh rob.vh uop.vh ram1r1w.sv ram2r1w.sv popcount.sv count_leading_zeros.sv unsigned_divider.sv fair_sched.sv ppa32.sv csa.sv rf4r2w.sv reg_ram1rw.sv l2.sv
 
 ifeq ($(UNAME_S),Linux)
-	CXX = clang++-12 -flto
+	CXX = clang++-16 -flto
 	MAKE = make
 	VERILATOR_SRC = /home/dsheffie/local/share/verilator/include/verilated.cpp
 	VERILATOR_VCD = /home/dsheffie/local/share/verilator/include/verilated_vcd_c.cpp
---
+++ convert_sv_to_v.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import glob
+import os
+import subprocess
+import re
+
+def main():
+    svs = glob.glob('*.sv')
+    if not os.path.isdir('verilog'):
+        os.mkdir('verilog')
+
+    outputs = []
+    modules = set()
+    for sv in svs:
+        module_names = []
+        with open(sv, 'r') as in_:
+            for line in in_:
+                m = re.search(r'module\s+(\w+)(#?)(\s+)?\(', line)
+                if m == None:
+                    continue
+                g = m.groups()
+                module_names.append(g[0])
+
+        for module in module_names:
+            if module in modules:
+                print('huh already seen %s, source %s' % (module, sv))
+            else:
+                modules.add(module)
+        
+        r = sv.split('.sv')[0]
+        v = r+'.v'
+        cmd = ['sv2v', sv, '--write=verilog/'+v, '-D=FPGA']
+        outputs.append('verilog/' + v)
+        subprocess.run(cmd)
+
+    with open('mipscore.v', 'w') as o:
+        for output in outputs:
+            with open(output, 'r') as in_:
+                for line in in_:
+                    o.write('%s' % line)
+                o.write('\n\n')
+
+if __name__ == "__main__":
+    main()
