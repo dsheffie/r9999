239da8ba8a5dfeaefe93649a379161eb9baf16f2
David Sheffield <sheffield.david@gmail.com>
2026-05-13 21:55:25 -0700

    change smt2 file name and deal with broken logic in l1i

--- l1i.sv
+++ l1i.sv
@@ -140,7 +140,7 @@ module compute_pht_idx(pc, hist, idx);
    wire [31:0] 			   w_fold_0 = hist[31:0] ^ hist[63:32];
    wire [15:0] 			   w_fold_1 = w_fold_0[31:16] ^ w_fold_0[15:0];
 
-   assign idx = w_fold_1  ^ pc[18:3];
+   assign idx = w_fold_1[`LG_PHT_SZ-1:0]  ^ pc[`LG_PHT_SZ+2:3];
    
 endmodule
 
